4bb7c238d0fee9ddd46ae37dcd0d6562eb09a7af
Paul Sonnentag <paul.sonnentag@googlemail.com>
2026-05-05 15:17:45 +0200

    create account doc with folders in one shot

--- packages/frame/src/account-context.tsx
+++ packages/frame/src/account-context.tsx
@@ -9,25 +9,20 @@ const STORAGE_KEY = "overlock-patchwork:root:account-url"
 export default defineView<AccountDoc>(async ({ element, repo }) => {
   let url = localStorage.getItem(STORAGE_KEY) as AutomergeUrl | null
   if (!url) {
-    url = repo.create<AccountDoc>({} as AccountDoc).url
+    const rootFolder = repo.create<FolderDoc>({ title: "root", docs: [] })
+    const packagesFolder = repo.create<FolderDoc>({
+      title: "packages",
+      docs: [],
+    })
+    const account = repo.create<AccountDoc>({
+      rootFolderUrl: rootFolder.url,
+      packagesFolderUrl: packagesFolder.url,
+    })
+    url = account.url
     localStorage.setItem(STORAGE_KEY, url)
   }
 
   const handle = await repo.find<AccountDoc>(url)
-  const doc = handle.doc()
-  if (!doc.rootFolderUrl) {
-    const folder = repo.create<FolderDoc>({ title: "root", docs: [] })
-    handle.change((d) => {
-      d.rootFolderUrl = folder.url
-    })
-  }
-  if (!doc.packagesFolderUrl) {
-    const packages = repo.create<FolderDoc>({ title: "packages", docs: [] })
-    handle.change((d) => {
-      d.packagesFolderUrl = packages.url
-    })
-  }
-
   Object.assign(element, { value: handle, handle, url })
   element.style.display = "contents"
   return undefined
